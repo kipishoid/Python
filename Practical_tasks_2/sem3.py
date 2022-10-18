e6f44c86497089d8480b95aedd8f208541b8b546
[sivolapov_max] <[kipish.max@mail.ru]>
2022-10-18 22:41:00 +0300

    second1

---
+++ Practical_tasks_2/sem3.py
@@ -0,0 +1,11 @@
+# from random import *
+# pred = [True, False]
+# kolvo = randint(5, 25)
+# predicates = [choice(pred) for _ in range(kolvo)]
+# print(predicates)
+
+
+import json
+
+BD = {'Иванов': {'adress': 'jkhfjdh', 'mail': 'dklfjdk',
+                 'phones': [98769876986798, 897987987987, 78978979879]}}
